b0735ec529a8692bf54664e333d0cf6085d0b3ef
Eduardo Antonio Cecilio Fernandes <eduardo.cecilio@gmail.com>
2015-11-17 22:03:17 -0200

    Updated existing code and added Mutex Account implementation.

--- MutexArticle/Program.cs
+++ MutexArticle/Program.cs
@@ -14,10 +14,12 @@ namespace MutexArticle
             {
                 if (args == null || args.Length == 0)
                 {
-                    throw new ArgumentException("There must be an arument for executing this application.");
+                    Console.WriteLine("There must be an arument for executing this application:");
+                    Console.WriteLine("NoLocking - execute application with no concurrency treatment.");
+                    Console.WriteLine("WithLocking - execute application with lock construct. It implements the Monitor design pattern.");
+                    Console.WriteLine("WithMutex - execute application with Mutex class. It implements the Monitor design pattern.");
                 }
-
-                if (args[0].Equals("NoLocking"))
+                else if (args[0].Equals("NoLocking"))
                 {
 
                     BankAccountSimple accountSimple = new BankAccountSimple(100);
@@ -34,6 +36,17 @@ namespace MutexArticle
 
                     Thread threadAdd_50 = new Thread(() => accountLock.AddMoney(50));
                     Thread threadWithdraw_110 = new Thread(() => accountLock.WithdrawMoney(110));
+
+                    threadAdd_50.Start();
+                    threadWithdraw_110.Start();
+                }
+                else if (args[0].Equals("WithMutex"))
+                {
+
+                    BankAccountMutex accountMutex = new BankAccountMutex(100);
+
+                    Thread threadAdd_50 = new Thread(() => accountMutex.AddMoney(50));
+                    Thread threadWithdraw_110 = new Thread(() => accountMutex.WithdrawMoney(110));
 
                     threadAdd_50.Start();
                     threadWithdraw_110.Start();
